d6e28881576182e101a0ef8b5744bada60c44cce
Masud99Rana <Masudrana917775@gmail.com>
2019-12-26 09:31:19 +0600

    delete feature for teacher & student implemented

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -66,9 +66,19 @@ class StudentController extends Controller
         return $this->createErrorResponse('The student with the specified id does not exists.', 404);
     }
 
-    public function destroy()
+    public function destroy($student_id)
     {
-        return __METHOD__;
+        $student = Student::find($student_id);
+
+        if($student)
+        {
+            $student->courses()->detach();
+            $student->delete();
+
+            return $this->createSuccessResponse("The student with id {$student_id} has been removed", 200);
+        }
+
+        return $this->createErrorResponse('The student with the specified id does not exists',404);
     }
 
     function validateRequest($request)
